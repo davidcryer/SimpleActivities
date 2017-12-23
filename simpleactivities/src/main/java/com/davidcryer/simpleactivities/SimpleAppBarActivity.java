7e719a0b4285a1ecde9fad481395db70ba78ddc4
David Cryer <davecryer.dc@gmail.com>
2017-12-23 21:26:49 +0000

    add methods to SimpleAppBarActivity that maps fragment util methods, but provides view container

--- simpleactivities/src/main/java/com/davidcryer/simpleactivities/SimpleAppBarActivity.java
+++ simpleactivities/src/main/java/com/davidcryer/simpleactivities/SimpleAppBarActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 
 public abstract class SimpleAppBarActivity extends SimpleFragmentManagerUtilsActivity {
@@ -34,6 +35,22 @@ public abstract class SimpleAppBarActivity extends SimpleFragmentManagerUtilsAct
     }
 
     protected abstract void addInitialFragment();
+
+    boolean hasFragment() {
+        return hasFragment(getContentFragmentViewContainer());
+    }
+
+    Fragment fragment() {
+        return fragment(getContentFragmentViewContainer());
+    }
+
+    protected void add(String tag, FragmentProvider fragmentProvider) {
+        add(getContentFragmentViewContainer(), tag, fragmentProvider);
+    }
+
+    protected void replace(String tag, FragmentProvider fragmentProvider) {
+        replace(getContentFragmentViewContainer(), tag, fragmentProvider);
+    }
 
     @IdRes
     protected int getContentFragmentViewContainer() {
